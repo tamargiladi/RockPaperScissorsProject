c0395e453dc3bbda648421f0752e1f8660bd7d29
Mohse Shina <mshinaaaa@gmail.com>
2021-05-15 16:35:55 +0300

    add serial connection

--- ExecuteHand.py
+++ ExecuteHand.py
@@ -3,21 +3,22 @@ import serial
 import time
 
 arduino = serial.Serial(port='COM4', baudrate=9600, timeout=.1)
+
 def write_read(x):
     arduino.write(bytes(x, 'utf-8'))
     time.sleep(0.05)
     data = arduino.readline()
     return data
-#while True:
-#    num = input("Enter a number: ") # Taking input from user
-#    value = write_read(num)
-#    print(value) # printing the value
+
+while True:
+    num = input("Enter a number: ") # Taking input from user
+    value = write_read(num)
+    print(value) # printing the value
 
 
-#arduinoData = serial.Serial('com4', 9600)
-
+'''
 def doRock():
-    value = write_read("s")
+    value = write_read("r")
     print(value)  # printing the value
 
 
@@ -27,8 +28,12 @@ def doScissors():
 
 
 def doPaper():
-    value = write_read("s")
+    value = write_read("p")
     print(value)  # printing the value
+'''
 
 
-doScissors()
+
+
+
+
