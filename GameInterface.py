bce3969d1a3601c705980b5d8a09adb21c22c899
Mohse Shina <mshinaaaa@gmail.com>
2021-05-24 02:24:55 +0300

    add serial connection

--- GameInterface.py
+++ GameInterface.py
@@ -4,13 +4,25 @@ import matplotlib.pyplot as plt
 from tkinter import *
 import GameSpace as GS
 
-def updateLabel(num):
-    label.config(str(num))
+
+
+
+
+
+
+#def updateLabel(num):
+#    label.config(str(num))
 
 root = Tk()
-label = Label(root,text="0")
+root.title("Rock Paper Scissors")
+computerLabel = Label(root, text="Computer", padx=40, pady=20) #columnspan=3
+playerLabel = Label(root, text="Player", padx=40, pady=20)
 
+computerLabel.grid(row=0, column=0)
+playerLabel.grid(row=0, column=1)
 
-root.after(0, GS.operate())
-label.pack()
+root.mainloop()
+
+#root.after(0, GS.operate())
+#label.pack()
 
