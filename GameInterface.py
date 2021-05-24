33ac65599b3f918f76bc9cd7bd8bccfcb418c505
Mohse Shina <mshinaaaa@gmail.com>
2021-05-25 00:11:55 +0300

    start making game Interface

--- GameInterface.py
+++ GameInterface.py
@@ -4,22 +4,57 @@ import matplotlib.pyplot as plt
 from tkinter import *
 import GameSpace as GS
 
-
-
-
-
-
-
 #def updateLabel(num):
 #    label.config(str(num))
 
+# game = GS.GameProgress()
+
+
+playerScore = 0
+playerMove= "Rock"
+computerScore = 0
+computerMove= "Paper"
+labelWidth = 20
+Color = 'aqua'
+game = GS.GameProgress()
+status = "Not ready!"  # (Not ready/Ready) get this from the gameSpace
+
+
 root = Tk()
 root.title("Rock Paper Scissors")
-computerLabel = Label(root, text="Computer", padx=40, pady=20) #columnspan=3
-playerLabel = Label(root, text="Player", padx=40, pady=20)
+root.configure(background=Color)
+#root.geometry("400x400")
 
-computerLabel.grid(row=0, column=0)
-playerLabel.grid(row=0, column=1)
+
+
+
+statusLabel = Label(root, text=status, pady=50, font=("Helvetica", 15), background=Color)
+statusLabel.grid(row=0, column=0, columnspan=2)
+
+computerMoveFrame = LabelFrame(root, text="Computer move", borderwidth=3, padx=30, pady=50, font=("Helvetica", 15), background=Color)
+computerMoveFrame.grid(row=1, column=0)
+computerMoveLabel = Label(computerMoveFrame, text=game.get_computer_move(), width=labelWidth, padx=10, pady=10, font=("Helvetica", 20))
+computerMoveLabel.pack()
+
+playerMoveFrame = LabelFrame(root, text="Player Move", borderwidth=3, padx=30, pady=50, font=("Helvetica", 15), background=Color)
+playerMoveFrame.grid(row=1, column=1)
+playerMoveLabel = Label(playerMoveFrame, text=playerMove, width=labelWidth, padx=10, pady=10, font=("Helvetica", 20))
+playerMoveLabel.pack()
+
+computerScoreFrame = LabelFrame(root, text="Computer Score", borderwidth=3, padx=30, pady=50, font=("Helvetica", 15), background=Color)
+computerScoreFrame.grid(row=2, column=0)
+computerScoreLabel = Label(computerScoreFrame, text=computerScore, width=labelWidth, padx=10, pady=10, font=("Helvetica", 20))
+computerScoreLabel.pack()
+
+
+playerScoreFrame = LabelFrame(root, text="Player Score", borderwidth=3, padx=30, pady=50, font=("Helvetica", 15), background=Color)
+playerScoreFrame.grid(row=2, column=1)
+playerScoreLabel = Label(playerScoreFrame, text=playerScore, width=labelWidth, padx=10, pady=10, font=("Helvetica", 20))
+playerScoreLabel.pack()
+
+
+start = Button(root, text="Start", borderwidth=3, padx=50, pady=10, font=("Helvetica", 15))
+start.grid(row=3, column=0, columnspan=2, pady=50)
 
 root.mainloop()
 
--- GameSpace.py
+++ GameSpace.py
@@ -42,11 +42,11 @@ numOfFingers_tmp = 3
 #==============
 
 class GameProgress:
-    def __init__(self, player_move,computer_move,score_computer=0,score_player=0):
-        self.player_move = player_move
-        self.computer_move = computer_move
-        self.score_computer = score_computer
-        self.score_player = score_player
+    def __init__(self, player_move='Rock', computer_move='Rock', score_computer=0, score_player=0):
+       self.player_move = player_move
+       self.computer_move = computer_move
+       self.score_computer = score_computer
+       self.score_player = score_player
 
     def get_player_move(self):
         return self.player_move
@@ -59,6 +59,141 @@ class GameProgress:
 
     def get_score_computer(self):
         return self.score_computer
+
+    def operate():
+        for i in range(12):
+            print("ENTERED!")
+        # todo:
+        #  remove later !!!!
+        printed = False
+
+        # todo================
+        text_color_hands = (0, 69, 255)
+        text_color_count = (209, 206, 0)
+
+        # Determines if the program should continue or not
+        running, countLeft, countRight = True, 0, 0
+
+        # detector = htm.handDetector(detectionCon=0.75)
+
+        KNN = ML.KNN_train(ML.get_table())
+        table_ML = ML.create_results_table()
+
+        # print(table_ML)
+
+        # Video conf
+
+        action = ""
+
+        ind_table = 0
+        countGame = 0
+
+        computerScore = 0
+        playerScore = 0
+
+        last_computer_action = ""
+        permitted_actions = [0, 2, 5]
+        for count in range(10):
+            while countGame < 10:
+                print("GO")
+
+                # --START---The single game operation
+
+                fg.startLoop()
+                # >>>>Camera detection
+                fingerCounter = fg.getCount()
+
+                #  #Calculates actions of the user
+                # if(fingerCounter==-2):
+                #     # cv2.putText(img, "NO HANDS", (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+                #     #             text_color_hands, 3)
+                #     running = True
+                # else:
+                #     if fingerCounter==0:
+                #         action = "Rock"
+                #     elif fingerCounter==4:
+                #         action = "Paper"
+                #     else:
+                #         action = "Scissors"
+                if fingerCounter in permitted_actions:
+                    if fingerCounter == 0:
+                        action = "Rock"
+                    elif fingerCounter == 5:
+                        action = "Paper"
+                    elif fingerCounter == 2:
+                        action = "Scissors"
+
+                    last_player_action = action
+
+                    # MACHINE LEARNING ALGORITHMS OR PSYCHOLOGY ALGORITHM
+
+                    if (countGame + 1) % 3 != 0:
+                        if (countGame + 1) % 3 == 1 and countGame != 0:
+                            ind_table = ind_table + 1  # increases table index when reaching to new triple game
+                        if countGame != 0:
+                            computer_action = SingleGame.psychology_beat(last_player_action, last_computer_action)
+                            last_computer_action = computer_action
+                        else:
+                            computer_action = "Paper"
+                            last_computer_action = "Paper"
+                        computer_action_NUM = ML.action_to_number(computer_action)
+                        action_NUM = ML.action_to_number(action)
+
+                        computer_action = ML.number_to_action(ML.KNN_prediction(table_ML, KNN)[ind_table])
+                        # print(int(action_NUM), int(computer_action_NUM), countGame, ind_table,
+                        #                                        table_ML)
+                        table_ML = ML.update_results_table(int(action_NUM), int(computer_action_NUM), countGame,
+                                                           ind_table,
+                                                           table_ML)
+
+                    else:
+                        # computer_action = ML.number_to_action()
+                        predicted_player_action = int(ML.KNN_prediction(table_ML, KNN)[ind_table])
+                        # print(ML.number_to_action(predicted_player_action))
+                        computer_action = SingleGame.beat_action(ML.number_to_action(predicted_player_action))
+
+                    # TODO: Saving database of
+
+                    # =======KEEP OUT
+
+                    if (computer_action == 'Rock'):
+                        print("doRock()")
+                        # EH.doRock()
+                    elif (computer_action == 'Scissors'):
+                        print("doScissors()")
+                        # EH.doScissors()
+                    elif (computer_action == 'Paper'):
+                        print("doPaper()")
+                        # EH.doPaper()
+
+                    if action != "INVALID ACTION":
+                        g_winner = SingleGame.is_winner(int(ML.action_to_number(action)),
+                                                        int(ML.action_to_number(computer_action)))
+                        winner = ""
+                        if g_winner == 1:
+                            winner = "player"
+                        elif g_winner == -1:
+                            winner = "computer"
+                        else:
+                            winner = "tie"
+
+                        if winner == "computer":
+                            computerScore = computerScore + 1
+                        elif winner == "player":
+                            playerScore = playerScore + 1
+
+                        print("winner:", winner, "player action:", action, "  computer action:", computer_action)
+
+                        countGame = countGame + 1
+                        time.sleep(3)
+
+            countGame = 0
+
+            print("========")
+            # print(ML.KNN_prediction(table_ML,KNN))
+            # print("player score:",playerScore,"computer score:", computerScore)
+            playerScore = 0
+            computerScore = 0
 
 
 # #=======FUNCTIONS SECTION=================
